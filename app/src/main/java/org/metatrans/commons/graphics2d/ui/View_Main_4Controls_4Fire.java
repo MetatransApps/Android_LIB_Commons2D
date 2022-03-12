cbd1f974a0ea04f2e09b8767026aca7f953e0c07
MetatransApps <kitopchiyski@gmail.com>
2022-03-13 00:17:30 +0200

    Draw joystick - player and acorn

--- app/src/main/java/org/metatrans/commons/graphics2d/ui/View_Main_4Controls_4Fire.java
+++ app/src/main/java/org/metatrans/commons/graphics2d/ui/View_Main_4Controls_4Fire.java
@@ -1,36 +1,42 @@
 package org.metatrans.commons.graphics2d.ui;
 
 
+import org.metatrans.commons.app.Application_Base;
 import org.metatrans.commons.graphics2d.app.Application_2D_Base;
 import org.metatrans.commons.graphics2d.main.Activity_Main_Base2D;
+import org.metatrans.commons.ui.utils.BitmapUtils;
 
-import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.RectF;
 import android.view.MotionEvent;
 
+import com.commons2d.R;
 
-@SuppressLint("ViewConstructor")
+
 public abstract class View_Main_4Controls_4Fire extends View_Main_Base {
 	
 	
-	private float control_move_X;
-	private float control_move_Y;
-	private float control_move_R;
-	private float control_move_X_touch;
-	private float control_move_Y_touch;
+	private float control_joystick_right_X;
+	private float control_joystick_right_R;
+	private float control_joystick_right_Y;
+	private float control_joystick_right_X_touch;
+	private float control_joystick_right_Y_touch;
 	
-	private float control_button1_X;
-	private float control_button1_Y;
-	private float control_button1_R;
-	
+	private float control_joystick_left_X;
+	private float control_joystick_left_R;
+	private float control_joystick_left_Y;
+
 	
 	private RectF control_player_bitmap;
 	private RectF control_shot_bitmap;
-	
-	
+	private RectF control_arrows_bitmap_1;
+	private RectF control_arrows_bitmap_2;
+
+	private Bitmap control_arrows;
+
+
 	private float[] control_move_vector = new float[2];
 	private float[] control_shot_vector = new float[2];
 	
@@ -46,27 +52,34 @@ public abstract class View_Main_4Controls_4Fire extends View_Main_Base {
 		super.initializeDimensions();
 		
 		float delim1 = 5.95f;
-		float delim2 = 7.95f;
+		//float delim2 = 7.95f;
 		
-		control_move_R = Math.min((getRectangle_Main().right - getRectangle_Main().left) / delim1, (getRectangle_Main().bottom - getRectangle_Main().top) / delim1);
-		control_button1_R = Math.min((getRectangle_Main().right - getRectangle_Main().left) / delim2, (getRectangle_Main().bottom - getRectangle_Main().top) / delim2);
+		control_joystick_right_R = Math.min((getRectangle_Main().right - getRectangle_Main().left) / delim1, (getRectangle_Main().bottom - getRectangle_Main().top) / delim1);
+		control_joystick_left_R = Math.min((getRectangle_Main().right - getRectangle_Main().left) / delim1, (getRectangle_Main().bottom - getRectangle_Main().top) / delim1);
 		
-		float control_margin = (4 * Math.min(control_move_R, control_button1_R)) / 5;
+		float control_margin = (4 * Math.min(control_joystick_right_R, control_joystick_left_R)) / 5;
 		
-		control_move_X = getRectangle_Main().right - (control_button1_R + control_margin);
-		control_move_Y = getRectangle_Main().bottom - (control_button1_R + control_margin);
+		control_joystick_right_X = getRectangle_Main().right - (control_joystick_left_R + control_margin);
+		control_joystick_left_Y = getRectangle_Main().bottom - (control_joystick_left_R + control_margin);
+		control_joystick_right_Y = getRectangle_Main().bottom - (control_joystick_right_R + control_margin);
+
+		control_joystick_right_X_touch = control_joystick_right_X;
+		control_joystick_right_Y_touch = control_joystick_left_Y;
+
+		control_joystick_left_X = control_joystick_right_R + control_margin;
+
 		
-		control_move_X_touch = control_move_X;
-		control_move_Y_touch = control_move_Y;
-		
-		control_button1_X = control_move_R + control_margin;
-		control_button1_Y = getRectangle_Main().bottom - (control_move_R + control_margin);
-		
-		
-		float bitmap1_size = control_move_R / 2;
-		float bitmap2_size = (2 * control_button1_R) / 3;
-		control_player_bitmap = new RectF(control_move_X - bitmap1_size, control_move_Y - bitmap1_size, control_move_X + bitmap1_size, control_move_Y + bitmap1_size);
-		control_shot_bitmap = new RectF(control_button1_X - bitmap2_size, control_button1_Y - bitmap2_size, control_button1_X + bitmap2_size, control_button1_Y + bitmap2_size);
+		float bitmap1_size = (1 * control_joystick_right_R) / 2;
+		float bitmap2_size = (1 * control_joystick_left_R) / 2;
+		float bitmap3_size = (1 * control_joystick_left_R) / 1;
+
+		control_player_bitmap = new RectF(control_joystick_right_X - bitmap1_size, control_joystick_right_Y_touch - bitmap1_size, control_joystick_right_X + bitmap1_size, control_joystick_left_Y + bitmap1_size);
+		control_shot_bitmap = new RectF(control_joystick_left_X - bitmap2_size, control_joystick_left_Y - bitmap2_size, control_joystick_left_X + bitmap2_size, control_joystick_left_Y + bitmap2_size);
+
+		control_arrows_bitmap_1 = new RectF(control_joystick_left_X - bitmap3_size, control_joystick_left_Y - bitmap3_size, control_joystick_left_X + bitmap3_size, control_joystick_left_Y + bitmap3_size);
+		control_arrows_bitmap_2 = new RectF(control_joystick_right_X - bitmap3_size, control_joystick_right_Y - bitmap3_size, control_joystick_right_X + bitmap3_size, control_joystick_right_Y + bitmap3_size);
+
+		control_arrows = BitmapUtils.fromResource(Application_Base.getInstance(), R.drawable.joystick_transparent);
 	}
 	
 	
@@ -79,47 +92,48 @@ public abstract class View_Main_4Controls_4Fire extends View_Main_Base {
 			
 			//Draw Controls
 			getDefaultPaint().setColor(Color.BLUE);
+			getDefaultPaint().setAlpha(61);
+			canvas.drawCircle(control_joystick_right_X, control_joystick_right_Y, control_joystick_right_R,	getDefaultPaint());
+			canvas.drawCircle(control_joystick_left_X,	control_joystick_left_Y, control_joystick_left_R,	getDefaultPaint());
+
 			getDefaultPaint().setAlpha(99);
-			canvas.drawCircle(control_move_X,		control_move_Y, control_move_R,	getDefaultPaint());
-			canvas.drawCircle(control_button1_X,	control_button1_Y, control_button1_R,	getDefaultPaint());
-			
-			///getDefaultPaint().setColor(Color.BLUE);
-			//getDefaultPaint().setAlpha(11);
-			//canvas.drawCircle(control_move_X,		control_move_Y, control_move_R - 5,	getDefaultPaint());
-			//canvas.drawCircle(control_button1_X,	control_button1_Y, control_button1_R - 5,	getDefaultPaint());
-			
+			canvas.drawBitmap(getBitmapControl_Arrows(), 	null, control_arrows_bitmap_1, getDefaultPaint());
+			canvas.drawBitmap(getBitmapControl_Arrows(), 	null, control_arrows_bitmap_2, getDefaultPaint());
+
 			getDefaultPaint().setAlpha(137);
 			canvas.drawBitmap(getBitmapControl_Player(), 	null, control_player_bitmap, getDefaultPaint());
 			canvas.drawBitmap(getBitmapControl_Shot(), 		null, control_shot_bitmap, getDefaultPaint());
-			
-			//if (control_move_X_touch != -1 && control_move_Y_touch != -1) {
-				
-				getDefaultPaint().setColor(Color.WHITE | Color.BLUE);
-				getDefaultPaint().setAlpha(177);
-				//getDefaultPaint().setColor(Color.BLUE);
-				//getDefaultPaint().setAlpha(255);
-				canvas.drawCircle(control_move_X_touch,		control_move_Y_touch, 	control_move_R / 10,	getDefaultPaint());
-			//}
+
+			//Draw point where the screen is touched
+			getDefaultPaint().setColor(Color.WHITE | Color.BLUE);
+			getDefaultPaint().setAlpha(137);
+			canvas.drawCircle(control_joystick_right_X_touch, control_joystick_right_Y_touch, 	control_joystick_right_R / 10,	getDefaultPaint());
 		}
 	}
 	
 	
 	protected abstract Bitmap getBitmapControl_Player();
+
 	protected abstract Bitmap getBitmapControl_Shot();
+
+	protected Bitmap getBitmapControl_Arrows() {
+
+		return control_arrows;
+	}
 	
 	
 	private void fillControlVector(float x, float y) {
 		
 		//In general, x and y must satisfy (x - center_x)^2 + (y - center_y)^2 < radius^2
-		float dx = x - control_move_X;
-		float dy = y - control_move_Y;
-		if (Math.pow(dx, 2) + Math.pow(dy, 2) < 2 * Math.pow(control_move_R, 2)) {
+		float dx = x - control_joystick_right_X;
+		float dy = y - control_joystick_left_Y;
+		if (Math.pow(dx, 2) + Math.pow(dy, 2) < 2 * Math.pow(control_joystick_right_R, 2)) {
 			
-			control_move_vector[0] = Math.min(1f, dx / control_move_R);
-			control_move_vector[1] = Math.min(1f, dy / control_move_R);
+			control_move_vector[0] = Math.min(1f, dx / control_joystick_right_R);
+			control_move_vector[1] = Math.min(1f, dy / control_joystick_right_R);
 			
-			control_move_X_touch = x;
-			control_move_Y_touch = y;
+			control_joystick_right_X_touch = x;
+			control_joystick_right_Y_touch = y;
 			
 			//System.out.println("control_vector[0]=" + control_move_vector[0] + ", control_vector[1]=" + control_move_vector[1]);
 		}
@@ -129,11 +143,11 @@ public abstract class View_Main_4Controls_4Fire extends View_Main_Base {
 	private void fillShotVector(float x, float y) {
 		
 		//In general, x and y must satisfy (x - center_x)^2 + (y - center_y)^2 < radius^2
-		float dx = x - control_button1_X;
-		float dy = y - control_button1_Y;
-		if (Math.pow(dx, 2) + Math.pow(dy, 2) < Math.pow(control_button1_R, 2)) {
-			control_shot_vector[0] = dx / control_button1_R;
-			control_shot_vector[1] = dy / control_button1_R;
+		float dx = x - control_joystick_left_X;
+		float dy = y - control_joystick_left_Y;
+		if (Math.pow(dx, 2) + Math.pow(dy, 2) < Math.pow(control_joystick_left_R, 2)) {
+			control_shot_vector[0] = dx / control_joystick_left_R;
+			control_shot_vector[1] = dy / control_joystick_left_R;
 			
 			//System.out.println("control_vector[0]=" + control_move_vector[0] + ", control_vector[1]=" + control_move_vector[1]);
 		}
@@ -141,9 +155,9 @@ public abstract class View_Main_4Controls_4Fire extends View_Main_Base {
 	
 	
 	private boolean isInsideShot(float x, float y) {
-		float dx = x - control_button1_X;
-		float dy = y - control_button1_Y;
-		return Math.pow(dx, 2) + Math.pow(dy, 2) < Math.pow(control_button1_R, 2);
+		float dx = x - control_joystick_left_X;
+		float dy = y - control_joystick_left_Y;
+		return Math.pow(dx, 2) + Math.pow(dy, 2) < Math.pow(control_joystick_left_R, 2);
 	}
 	
 	
